8ebebfb9b85d92239d389b2f6a2330412afe3fc3
Nikita <nikita.serg.utkin@gmail.com>
2023-01-10 22:11:38 +0900

    finalwork

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+Console.Write("Введите элементы через пробел: ");
+string[] M = Console.ReadLine().Split(' ');
+string[] N = new string[M.Length];
+int i = 0;
+int j = 0;
+int size = 0;
+for (i = 0; i < M.Length; i++)
+    Console.Write($"{M[i]} ");
+Console.WriteLine();
+for (i = 0; i < M.Length; i++)
+    if (M[i].Length <= 3)
+    {
+        N[j] = M[i];
+        j++;
+        i++;
+    }
+    else i++;
+size = j;
+for (j = 0; j < size; j++)
+    Console.Write($"{N[j]} ");
+Console.WriteLine();
+
